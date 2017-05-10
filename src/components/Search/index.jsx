ae364259066e5a99fb60339601d0a83496ed3f34
haozhaohang <745184644@qq.com>
2017-05-10 15:00:25 +0800

    feat: update Search Component

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // css
 import './index.styl';
 
-const Search = () =>
-    (
-        <section className="search-wrapper">
-            <from>
-                <input className="text" placeholder="请输入搜索关键字" />
-                <button className="btn" >搜索</button>
-            </from>
-        </section>
-    );
+class Search extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ''
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(e) {
+        const value = e.target.value;
+
+        this.state = {
+            value
+        };
+    }
+
+    handleSubmit() {
+        const { value } = this.state;
+        const { onSearch } = this.props;
+
+        onSearch(value);
+    }
+
+    render() {
+        return (
+            <section className="search-wrapper">
+                <from>
+                    <input className="text" placeholder="请输入搜索关键字" onChange={val => this.handleInputChange(val)} />
+                    <button className="btn" onClick={this.handleSubmit}>搜索</button>
+                </from>
+            </section>
+        );
+    }
+}
 
 export default Search;
